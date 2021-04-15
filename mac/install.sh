8fa26f7d61f50409ca596f71ffb578b1818a6d57
davidgaida <david.gaida@tu-dortmund.de>
2021-04-15 20:59:16 +0200

    add install.sh

---
+++ mac/install.sh
@@ -0,0 +1,26 @@
+#Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Install Mac App Store command line interface https://github.com/mas-cli/mas
+brew install mas
+
+#Install git
+brew install git
+
+#Install Homebrew cask
+brew cask
+
+#Install tools
+brew install nvm
+
+#Install browsers
+brew cask install google-chrome
+
+#Install dev tools
+brew cask install visual-studio-code
+brew cask install intellij-idea
+brew cask install textmate
+
+#Install messaging apps
+brew cask install whatsapp
+brew cask install telegram
